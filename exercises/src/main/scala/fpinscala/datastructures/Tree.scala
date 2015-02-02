01db685d4f3bc7dd224a6186d4da3e2d17d8aabc
Sam Goldman <samwgoldman@gmail.com>
2015-02-01 21:35:03 -1000

    Exercise 3.29

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,27 +6,27 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
-  def size[A](t: Tree[A]): Int =
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B =
     t match {
-      case Leaf(_) => 1
-      case Branch(l, r) => size(l) + size(r)
+      case Leaf(x) => f(x)
+      case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
     }
+
+  def size[A](t: Tree[A]): Int = {
+    val one = Function.const(1) _
+    fold(t)(one)(_ + _)
+  }
 
   def max(t: Tree[Int]): Int =
-    t match {
-      case Leaf(x) => x
-      case Branch(l, r) => max(l) max max(r)
-    }
+    fold(t)(identity)(_ max _)
 
-  def depth[A](t: Tree[A]): Int =
-    t match {
-      case Leaf(_) => 1
-      case Branch(l, r) => 1 + (depth(l) max depth(r))
-    }
+  def depth[A](t: Tree[A]): Int = {
+    val one = Function.const(1) _
+    fold(t)(one)(1 + _.max(_))
+  }
 
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
-    t match {
-      case Leaf(x) => Leaf(f(x))
-      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
-    }
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = {
+    val leaf: A => Tree[B] = x => Leaf(f(x))
+    fold(t)(leaf)(Branch(_, _))
+  }
 }
