f835c1f0d4abe1b4640012353a93f11ad172c79e
Sam Goldman <samwgoldman@gmail.com>
2015-02-01 20:25:31 -1000

    Exercise 3.18

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -113,5 +113,9 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(x, xs) => Cons(x.toString, doubleStrings(xs))
     }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def map[A,B](l: List[A])(f: A => B): List[B] =
+    l match {
+      case Nil => Nil
+      case Cons(x, xs) => Cons(f(x), map(xs)(f))
+    }
 }
