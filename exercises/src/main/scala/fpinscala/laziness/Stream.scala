8e12bcc577fa035900e5da47b0af2207e0e7c719
Sam Goldman <samwgoldman@gmail.com>
2015-02-01 23:13:17 -1000

    Exercise 5.4

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -39,7 +39,8 @@ trait Stream[+A] {
       case _ => empty
     }
 
-  def forAll(p: A => Boolean): Boolean = sys.error("todo")
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
 }
