12d119b68fb141e30a7df5254987612beaf4b090
Sam Goldman <samwgoldman@gmail.com>
2015-02-01 19:27:28 -1000

    Exercise 3.3

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -53,7 +53,7 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(x, xs) => xs
     }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = Cons(h, l)
 
   def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
