8def151b68837a8ccbc80e109b8136bc8e4bebb0
Sam Goldman <samwgoldman@gmail.com>
2015-02-01 22:21:51 -1000

    Exercise 4.4

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -57,7 +57,11 @@ object Option {
       y <- b
     } yield f(x, y)
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case Nil => Some(Nil)
+      case x :: xs => map2(x, sequence(xs))(_ :: _)
+    }
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
 }
