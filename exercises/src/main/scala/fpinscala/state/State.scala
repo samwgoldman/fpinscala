7da63646c2765c32e5981af5266355b8df84160f
Sam Goldman <samwgoldman@gmail.com>
2015-02-04 19:53:01 -1000

    Exercise 6.4

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -59,7 +59,15 @@ object RNG {
     ((d1, d2, d3), r3)
   }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count <= 0)
+      (List(), rng)
+    else {
+      val (i, r1) = rng.nextInt
+      val (l, r2) = ints(count - 1)(r1)
+      (i :: l, r2)
+    }
+  }
 
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
